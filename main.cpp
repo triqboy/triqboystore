7c09a933e40c37570b54a45e38a4e2e4f3598b6a
triqboy <gigotriqboy@gmail.com>
2014-10-24 10:28:01 +0400

    new

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "sdt.h"
+
+int main()
+{ int a,b,c;
+    cout<<"Enter the number 1: ";
+    cin>>a;
+    cout<<"Enter the number 2: ";
+    cin>>b;
+    c=a+b;
+    cout<<"Sum = "<<c<<endl;
+}
