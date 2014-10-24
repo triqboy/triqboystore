d9d6cf7b3207bb953c3095ae6c5cd1238c8086d0
triqboy <gigotriqboy@gmail.com>
2014-10-24 10:57:06 +0400

    true

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "sdt.h"
 
 int main()
-{ int a,b,c;
+{ int a,b,c,r;
     cout<<"Enter the number 1: ";
     cin>>a;
     cout<<"Enter the number 2: ";
     cin>>b;
     c=a+b;
+    r=a-b;
     cout<<"Sum = "<<c<<endl;
+    cout<<"Diff = "<<r<<endl;
 }
